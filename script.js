fec506ea0ab5f3d49c7984675e623c4fc84c7f7d
tugwellchristi <tugwellchristi@gmail.com>
2023-09-30 15:18:10 -0700

    styling and intializing JS

---
+++ script.js
@@ -0,0 +1,79 @@
+const questions = [
+{
+    question: "Where is the correct place to insert a Javascript?", 
+    answers: [
+        { text: "The head section", correct: false}, 
+        { text: "The body section", correct: false},
+        { text: "The head and body section are correct", correct: true},
+    ]
+}, 
+{
+    question: "How do you write an IF statement in JavaScript?", 
+    answers: [
+        { text: "if (i == 5)", correct: true}, 
+        { text: "if i = 5 then", correct: false},
+        { text: "if i == 5 then", correct: false},
+    ]
+}, 
+{
+    question: "Which operator is used to assign a value to a variable?", 
+    answers: [
+        { text: "-", correct: false}, 
+        { text: "X", correct: false},
+        { text: "=", correct: true},
+    ]
+}, 
+{
+    question: "What will the following code return: Boolean(10 > 9)", 
+    answers: [
+        { text: "false", correct: false}, 
+        { text: "true", correct: true},
+        { text: "NAN", correct: false},
+    ]
+}, 
+{
+    question: "Which event occurs when the user clicks on an HTML element?", 
+    answers: [
+        { text: "onmouseclick", correct: false}, 
+        { text: "click", correct: false},
+        { text: "onclick", correct: true},
+    ]
+}, 
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);  
+    });
+}
+
+function resetState() {
+    nextButton.style.display ="none";
+    while(answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+startQuiz();
